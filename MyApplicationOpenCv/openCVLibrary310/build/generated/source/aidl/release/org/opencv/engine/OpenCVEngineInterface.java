a6fd6b526396cc904217075994ad9a16d8564d43
saurabhsircar11 <saurabhsircar11@gmail.com>
2017-01-04 13:41:43 +0530

    checkout

--- MyApplicationOpenCv/openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ MyApplicationOpenCv/openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/sever/Documents/StudioProjects/MyApplicationOpenCv/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/sever/Experiments/Experiments/MyApplicationOpenCv/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
